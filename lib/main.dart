8d6a4f4e88d0fca64bc6292bcd97c472ecad17d7
Anoop Jose <anoopjosearackal@icloud.com>
2020-07-26 08:07:09 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           softWrap: true,
                         ),
                       ),
-
                     ],
                   )),
 
